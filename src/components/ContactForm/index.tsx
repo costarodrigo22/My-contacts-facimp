bb7326e91bcfd3154296511f7b22b164ae5daf52
rodrigo_motocasystems <rodrigo@motocasystems.com.br>
2023-02-15 23:29:03 -0300

    last commit

--- src/components/ContactForm/index.tsx
+++ src/components/ContactForm/index.tsx
@@ -1,23 +1,47 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Button from '../Button';
 import { FormGroup } from '../FormGroup';
 import Input from '../Input';
 import Select from '../Select';
 import { Container } from './styles';
 
-interface TypeContactForm {
-  buttonLabel: string,
+interface TypeOnSubmitFunction {
+  name: string,
+  email: string,
+  telephone: string,
+  categories_id?: number
 }
 
-export function ContactForm({ buttonLabel }: TypeContactForm){
+interface TypeContactForm {
+  buttonLabel: string,
+  onSubmit: (fomrData: TypeOnSubmitFunction) => void
+}
+
+interface TypeListCategory {
+  id: number,
+  name: string,
+  created_at: string,
+  updated_at: string,
+}
+
+export function ContactForm({ buttonLabel, onSubmit }: TypeContactForm){
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [phone, setPhone] = useState('');
-  const [category, setCategory] = useState('');
+  const [category, setCategory] = useState<number>();
+  const [categories, setCategories] = useState<TypeListCategory[]>();
+
+  useEffect(() => {
+    fetch('http://localhost:2000/api/category/index')
+      .then(async (response) => {
+        const json = await response.json();
+        setCategories(json.data);
+      });
+  }, []);
 
   function handleSubmit(event: React.FormEvent<HTMLFormElement>){
     event.preventDefault();
-    console.log({ name, email, phone, category });
+    onSubmit({ name, email, telephone: phone, categories_id: category });
   }
 
   return(
@@ -46,11 +70,15 @@ export function ContactForm({ buttonLabel }: TypeContactForm){
       <FormGroup >
         <Select
           value={category}
-          onChange={(event) => setCategory(event.target.value)}
+          onChange={(event) => setCategory(Number(event.target.value))}
         >
-          <option value=''>Categoria</option>
-          <option value='Instagram'>Instagram</option>
-          <option value='Facebook'>Facebook</option>
+          <option value=''>Selecione uma categoria</option>
+
+          {
+            categories?.map(item => (
+              <option key={item.id} value={item.id}>{item.name}</option>
+            ))
+          }
         </Select>
       </FormGroup>
       <FormGroup >
--- src/pages/NewContact/index.tsx
+++ src/pages/NewContact/index.tsx
@@ -1,11 +1,33 @@
 import { ContactForm } from '../../components/ContactForm';
 import PageHeader from '../../components/PageHeader';
 
+interface TypeBodyRequestAddNewContact {
+  name: string,
+  email: string,
+  telephone: string,
+  categories_id?: number
+}
+
 export default function NewContact(){
+  function handleSubmit(formData: TypeBodyRequestAddNewContact){
+    const options = {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(formData),
+    };
+
+    fetch('http://localhost:2000/api/contact/store', options)
+      .then(async (response) => {
+        console.log(response);
+      });
+  }
   return(
     <>
       <PageHeader title='Novo contato'/>
-      <ContactForm buttonLabel='Cadastrar'/>
+      <ContactForm
+        onSubmit={handleSubmit}
+        buttonLabel='Cadastrar'
+      />
     </>
   );
 }
